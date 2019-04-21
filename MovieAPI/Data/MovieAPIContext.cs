f3f5dfccfa67c25f3fe4a951d16beb8b178631d9
Sena KILICARSLAN <sena_kilicarslan@yahoo.com>
2019-04-21 12:51:52 +0300

    Add project files.

---
+++ MovieAPI/Data/MovieAPIContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieAPI.Models
+{
+    public class MovieAPIContext : DbContext
+    {
+        public MovieAPIContext (DbContextOptions<MovieAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
